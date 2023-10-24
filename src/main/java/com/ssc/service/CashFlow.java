343a8799d2f5ecd40466638e6d783e7f7fc9960e
Wilkins, Bruce <Bruce.Wilkins@sscinc.com>
2023-10-24 12:32:31 -0400

    Initial commit

---
+++ src/main/java/com/ssc/service/CashFlow.java
@@ -0,0 +1,93 @@
+package com.ssc.service;
+
+
+import java.time.Month;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class CashFlow {
+    private  final  String month;
+    private final double cashFlow;
+    private final int year;
+    private  final int dayOfMonth;
+
+    private static final Map<String, Integer> daysInMonth = new HashMap<>();
+
+    static {
+        daysInMonth.put("January", 31);
+        daysInMonth.put("February", 28); // Default to 28 days for non-leap years.
+        daysInMonth.put("March", 31);
+        daysInMonth.put("April",30);
+        daysInMonth.put("May",31);
+        daysInMonth.put("June",30);
+        daysInMonth.put("July",31);
+        daysInMonth.put("August",31);
+        daysInMonth.put("September",30);
+        daysInMonth.put("October",31);
+        daysInMonth.put("November",30);
+        daysInMonth.put("December",31);
+
+        // Add the rest of the months
+    }
+
+    public CashFlow(String month, double cashFlow, int dayOfMonth, int year) {
+        this.month = month;
+        this.cashFlow = cashFlow;
+        this.year = year;
+        this.dayOfMonth = dayOfMonth;
+    }
+
+    public double calculateWeightedCashFlow(CashFlow cashFlows) {
+        double days = daysInMonth.get(month);
+        System.out.println("days "+days);
+        if (month.equals("February") && isLeapYear(year)) {
+            days = 29; // Adjust for leap year.
+        }
+        double ff = (days - cashFlows.dayOfMonth)/ days;
+        System.out.println("Computeres WCH without cashFlows" + ff);
+        double weightedCashFlow = cashFlow *  (( days - cashFlows.dayOfMonth)/ days);
+        System.out.println("days from calculatedWeighted " + days);
+        System.out.println("cashFlow " + cashFlow);
+        System.out.println("cashFlows " + cashFlows.dayOfMonth);
+        System.out.println("computed weightedCashFlow" + weightedCashFlow);
+        //return cashFlow *  (( days - cashFlows.dayOfMonth)/ days);
+        return weightedCashFlow;
+    }
+
+    public double calculateDollarWeightedCashFlow(CashFlow cashFlows) {
+
+        return ((calculateWeightedCashFlow(cashFlows) / 3 * (3 - getMonthNumber(month) + 1)) + (calculateWeightedCashFlow(cashFlows) / 3 * ((cashFlow - (calculateWeightedCashFlow(cashFlows))/3) * (3 - getMonthNumber(month)))));
+
+    }
+    public  double computeTotalCashFlow(CashFlow cashFlows) {
+
+        return cashFlows.cashFlow;
+    }
+    public void displayCashFlows(List<CashFlow> cashFlows) {
+            cashFlows.stream()
+                .forEach(System.out::println);
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+    public static int getMonthNumber(String monthName) {
+        try {
+            Month month = Month.valueOf(monthName.toUpperCase());
+            return month.getValue();
+        } catch (IllegalArgumentException e) {
+            // Handle invalid input (month names that don't exist)
+            return -1; // You can choose a different way to handle this error
+        }
+    }
+
+
+
+    private int getTotalDaysInMonth(int year) {
+        if (month.equals("February") && isLeapYear(year)) {
+            return 29;
+        }
+        return daysInMonth.get(month);
+    }
+}
---
+++ src/main/java/com/ssc/service/SimultaneousTimeDollarWeighting.java
@@ -0,0 +1,74 @@
+package com.ssc.service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimultaneousTimeDollarWeighting {
+
+    public static void main(String[] args) {
+        double initialBalance = 1000.0;
+        double endingBalance = 1200.0;
+        int year = 2021;
+
+        List<CashFlow> cashFlows = createSampleCashFlows();
+
+
+        double totalWeightedCashFlow = calculateWeightedCashFlows(cashFlows);
+        //double totalWeightedCashFlow = cashFlows.stream().mapToDouble(n -> n.calculateWeightedCashFlow(n)).sum();
+        double totalDollarWeightedCashFlow = cashFlows.stream().mapToDouble(n -> n.calculateDollarWeightedCashFlow(n)).sum();
+        double totalCashFlow = calculateTotalCashFlow(cashFlows);
+        System.out.println("initial balance "+ initialBalance);
+        System.out.println("ending Blance " + endingBalance);
+        System.out.println("total cashflow " + totalCashFlow);
+        System.out.println("totalWeightedCashFlow " + totalWeightedCashFlow);
+        System.out.println("totalDollarWeightedCashFlow " + totalDollarWeightedCashFlow);
+        System.out.println("totalCashFlow " + totalCashFlow);
+
+        double calculatedRateOfReturn = calculateModifiedDietzRate(initialBalance, endingBalance, totalCashFlow, cashFlows, totalDollarWeightedCashFlow);
+
+        System.out.println("Quarterly Rate of Return: " + (calculatedRateOfReturn * 100) + "%");
+    }
+
+    private static List<CashFlow> createSampleCashFlows() {
+        List<CashFlow> cashFlows = new ArrayList<>();
+        cashFlows.add(new CashFlow("January", 10.0,15,2021));
+        cashFlows.add(new CashFlow("February", 50.0,7,2021));
+        cashFlows.add(new CashFlow("March", 20.0,20,2021));
+        return cashFlows;
+    }
+
+    private static double calculateWeightedCashFlows(List<CashFlow> cashFlows) {
+        double totalWeightedCashFlow = 0.0;
+        for (CashFlow cashFlow : cashFlows) {
+            totalWeightedCashFlow += cashFlow.calculateWeightedCashFlow(cashFlow);
+        }
+        return totalWeightedCashFlow;
+    }
+    private static double calculateTotalCashFlow(List<CashFlow> cashFlowList) {
+        double totalCashFlows =0.0;
+        for (CashFlow cashFlow : cashFlowList) {
+            totalCashFlows = cashFlow.computeTotalCashFlow(cashFlow);
+        }
+        return totalCashFlows;
+    }
+
+
+    private static double calculateDollarWeightedCashFlows(List<CashFlow> cashFlows) {
+        double totalDollarWeightedCashFlow = 0.0;
+        for (CashFlow cashFlow : cashFlows) {
+            totalDollarWeightedCashFlow += cashFlow.calculateDollarWeightedCashFlow(cashFlow);
+            System.out.println("Summation of totalDollarWeightedCashFlow "+totalDollarWeightedCashFlow);
+        }
+        System.out.println("totalDollarWeightedCashFlow ="+totalDollarWeightedCashFlow);
+        return totalDollarWeightedCashFlow;
+    }
+
+    private static double calculateModifiedDietzRate(double initialBalance, double endingBalance, double totalCashFlow, List<CashFlow> cashFlows, double totalDollarWeightedCashFl) {
+        System.out.println("Dietzrate return totalDollarWeightedCashFl " + totalDollarWeightedCashFl);
+        return (endingBalance - initialBalance - totalCashFlow) / (initialBalance + totalDollarWeightedCashFl);
+    }
+}
+
+
+
